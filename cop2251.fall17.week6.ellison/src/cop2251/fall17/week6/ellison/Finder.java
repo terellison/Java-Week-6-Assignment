74618eb295db493932c20df3e4c18cb1f3c88470
Terry Ellison <ellisonterry4@gmail.com>
2017-11-16 17:14:46 -0500

    added comments to increase readability

--- cop2251.fall17.week6.ellison/src/cop2251/fall17/week6/ellison/Finder.java
+++ cop2251.fall17.week6.ellison/src/cop2251/fall17/week6/ellison/Finder.java
@@ -1,3 +1,5 @@
+// Terry Ellison SPC ID: 2335229
+
 package cop2251.fall17.week6.ellison;
 
 
@@ -20,10 +22,16 @@ public class Finder {
 		
 	
 	public int findMax(String s, int max) {
+		
+		// check if the length of the string is zero; this is our recursion fail-safe
 		if(s.length() == 0) {
 			return max;
 		}
 		
+		/* Checks if there is more than one character in the given string,
+		 * if so then builds a string to hold every consecutive number after our start point.
+		 * This is how we have multi-digit numbers instead of single integers.
+		 * (i.e. 12 instead of comparing 1, then 2 on the next iteration) */
 		
 		if(s.length() > 1) {
 			if(Character.isDigit(s.charAt(0))) {
@@ -37,9 +45,20 @@ public class Finder {
 					else
 						charIndex++;
 				}
+				
+				// after our string of numbers is built we convert to an integer for comparison purposes
+				
 				String number = numString.toString();
 				
 				int wholeNumber = Integer.valueOf(number);
+
+				/* Compares our newly built integer to the current maximum
+				 * that was passed in. If our number is greater than the aforementioned max,
+				 * then our integer becomes the new max and the function is called again with
+				 * the updated maximum. Otherwise, the function is called again with the max
+				 * and is given a substring starting at the stop position of our number building
+				 * loop. */
+				
 				
 				if(Character.isDigit(s.charAt(0))) {
 					if(wholeNumber > max) {
@@ -54,6 +73,11 @@ public class Finder {
 				}
 			}
 		}
+		
+		/* Our scenario when we have reached the last character in the string;
+		 * checks if the character is a digit and appropriately compares it to
+		 * the current max as needed.
+		 */
 		
 		else {
 			if(Character.isDigit((s.charAt(0)))) {
