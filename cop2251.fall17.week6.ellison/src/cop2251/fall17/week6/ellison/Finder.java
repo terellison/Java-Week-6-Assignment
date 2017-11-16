bf4dcd94a4f54343717d2e9586e905727a78e981
Terry Ellison <ellisonterry4@gmail.com>
2017-11-16 16:48:23 -0500

    implemented findMax() and it actually works :)

--- cop2251.fall17.week6.ellison/src/cop2251/fall17/week6/ellison/Finder.java
+++ cop2251.fall17.week6.ellison/src/cop2251/fall17/week6/ellison/Finder.java
@@ -1,5 +1,6 @@
 package cop2251.fall17.week6.ellison;
 
+
 public class Finder {
 
 	public int sumIt(String s) {
@@ -19,6 +20,51 @@ public class Finder {
 		
 	
 	public int findMax(String s, int max) {
-		return 0;
+		if(s.length() == 0) {
+			return max;
+		}
+		
+		
+		if(s.length() > 1) {
+			if(Character.isDigit(s.charAt(0))) {
+				StringBuilder numString = new StringBuilder();
+				int charIndex = 0;
+				while(Character.isDigit(s.charAt(charIndex)) && charIndex <= (s.length() - 1)) {
+					numString.append(s.charAt(charIndex));
+					if((charIndex + 1) > (s.length() - 1)) {
+						break;
+					}
+					else
+						charIndex++;
+				}
+				String number = numString.toString();
+				
+				int wholeNumber = Integer.valueOf(number);
+				
+				if(Character.isDigit(s.charAt(0))) {
+					if(wholeNumber > max) {
+						max = wholeNumber;
+						if((s.length() - number.length()) == 0) {
+							return max;
+						}
+						return findMax(s.substring(s.length() - number.length()), max);
+					}
+					else
+						return findMax(s.substring(1), max);
+				}
+			}
+		}
+		
+		else {
+			if(Character.isDigit((s.charAt(0)))) {
+				if(Character.getNumericValue(s.charAt(0)) > max) {
+					max = Character.getNumericValue(s.charAt(0));
+					return max;
+				}
+				else
+					return max;
+			}
+		}
+		return findMax(s.substring(1), max);
 	}
 }
